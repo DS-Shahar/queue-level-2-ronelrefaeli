995115578617a4e7de7664bc371a8d746760e6f2
ronelrefaeli <ronel.refaeli1605@gmail.com>
2026-02-02 15:57:55 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,14 @@
 
-    public static int EX12(BinNode<Integer> node) {
+    public static int EX12(BinNode<Integer> node)
+    {
         if (node == null)
             return 0;
         int c = (node.getValue() >= 10 && node.getValue() < 100) ? 1 : 0;
         return c + EX12(node.getLeft()) + EX12(node.getRight());
     }
 
-    public static int EX14(BinNode<?> node) {
+    public static int EX14(BinNode<?> node)
+    {
         if (node == null)
             return 0;
         if (!node.hasLeft() && !node.hasRight())
@@ -14,7 +16,8 @@
         return EX14(node.getLeft()) + EX14(node.getRight());
     }
 
-    public static int EX16(BinNode<Integer> node) {
+    public static int EX16(BinNode<Integer> node)
+    {
         if (node == null)
             return 0;
         int sum = 0;
@@ -23,14 +26,43 @@
         return sum + EX16(node.getLeft()) + EX16(node.getRight());
     }
 
-    public static int EX17(BinNode<Double> node) {
+    public static int EX17(BinNode<Double> node)
+        {
         if (node == null)
             return 0;
         int c = 0;
-        if (node.hasLeft() && node.hasRight() &&
-            node.getLeft().getValue() % 1 != 0 &&
-            node.getRight().getValue() % 1 != 0)
+        if (node.hasLeft() && node.hasRight() && node.getLeft().getValue() % 1 != 0 && node.getRight().getValue() % 1 != 0)
             c = 1;
         return c + EX17(node.getLeft()) + EX17(node.getRight());
     }
 }
+
+
+
+public static void ex1(BinNode<Integer> node)
+    {
+    int count1=0:
+     int count2=0:   
+    if (node==null)
+        return 0;
+    BinNode <Integer> n=node
+    while (node!=null)
+        {
+            count1++;
+            node.getLeft());
+        }
+    while (n!=null)
+        {
+         
+            count2++;
+            n.getLeft();
+        }
+    return count1+count2;
+}
+    
+    
+            
+    
+
+
+
